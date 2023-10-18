57ba8aa635f3a96e5cf51245102156d1030cd3f0
A. Shustovsky <a.shustovsky@gmail.com>
2023-10-18 11:35:25 +0400

    init: start project

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+export function App() {
+  return (
+    <div className="w-full h-[100vh] pt-32 bg-[#161616] text-white">
+      <div className="mx-40">
+        <h1>Hello!</h1>
+      </div>
+    </div>
+  )
+}
