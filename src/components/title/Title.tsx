7cb22b4ac2735a884b79ecfeff456fcf8b915fa0
A. Shustovsky <a.shustovsky@gmail.com>
2023-10-18 11:57:02 +0400

    feat: add Title component

---
+++ src/components/title/Title.tsx
@@ -0,0 +1,37 @@
+import {Heading} from "@radix-ui/themes";
+import {clsx} from "clsx";
+
+export enum HeadingLevel {
+  h1 = "h1",
+  h2 = "h2",
+  h3 = "h3",
+  h4 = "h4",
+  h5 = "h5",
+  h6 = "h6",
+}
+
+interface TitleProps {
+  className?: string;
+  level: HeadingLevel;
+  text: string;
+}
+
+export const Title = (props: TitleProps) => {
+  const levelClasses = {
+    h1: "text-5xl",
+    h2: "text-3xl",
+    h3: "text-1xl",
+    h4: "text-xl",
+    h5: "text-base",
+    h6: "text-xs",
+  };
+
+  return (
+    <Heading
+      as={props.level}
+      className={clsx(levelClasses[props.level], props.className)}
+    >
+      {props.text}
+    </Heading>
+  );
+};
