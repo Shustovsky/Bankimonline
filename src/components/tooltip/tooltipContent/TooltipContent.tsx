611aa489d98b7eb18c0ab2aec31f7ed78b020984
A. Shustovsky <a.shustovsky@gmail.com>
2023-10-19 10:28:53 +0400

    fix: change style

--- src/components/tooltip/tooltipContent/TooltipContent.tsx
+++ src/components/tooltip/tooltipContent/TooltipContent.tsx
@@ -12,11 +12,12 @@ export const TooltipContent = (props: TooltipContentProps) => {
   return (
     <RadixPortal>
       <RadixContent
-        className="data-[state=delayed-open]:data-[side=top]:animate-slideDownAndFade data-[state=delayed-open]:data-[side=right]:animate-slideLeftAndFade data-[state=delayed-open]:data-[side=left]:animate-slideRightAndFade data-[state=delayed-open]:data-[side=bottom]:animate-slideUpAndFade text-violet11 select-none rounded-[4px] bg-white px-[15px] py-[10px] text-[15px] leading-none shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] will-change-[transform,opacity]"
+        className="bg-[#41434e] shadow-[0px_8px_16px_0px_rgba(0,_0,_0,_0.12)] w-full max-w-[250px] px-2 py-1 rounded text-white"
         sideOffset={5}
+        align={"start"}
       >
         {props.text}
-        <RadixArrow className="fill-white"/>
+        <RadixArrow className="fill-[#41434E]"/>
       </RadixContent>
     </RadixPortal>
   )
